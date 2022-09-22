e1ba74f134cdfe2887dc4b690d4e060a5e5d9990
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-22 22:32:17 +0200

    initial project, set up, create addUser component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const AddUser = () => {
+
+    const addUserHandler = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">UserName</label>
+            <input id="username" type='text' />
+            <label htmlFor="age">Age</label>
+            <input id="age" type='number' />
+            <button type="submit">Add User</button>
+        </form>
+    )
+}
