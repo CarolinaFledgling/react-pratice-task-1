9581924e9f27c4739862d4fd2b7282ebbaff0409
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-23 12:14:42 +0200

    added reused button component, work on style

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '../UI/Button'
 import { Card } from '../UI/Card'
 import styled from './AddUser.module.css'
 
@@ -14,7 +15,7 @@ export const AddUser = () => {
                 <input id="username" type='text' />
                 <label htmlFor="age">Age</label>
                 <input id="age" type='number' />
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
 
