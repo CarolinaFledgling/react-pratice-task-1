6e1aff641c13653f094fb3ac7aef1acebd40f15f
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-04 14:46:59 +0200

    added localstorage to store data in the  web storage update readme

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import { UserList } from "./components/Users/UserList";
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [usersList, setUsersList] = useState(() => {
+    const localStorageValue = localStorage.getItem('users')
+    if (localStorageValue) {
+      const users = JSON.parse(localStorageValue)
+      return users
+    }
+    return []
+  });
 
   const addUserHandler = (userName, userSurname, age) => {
 
     console.log(userName, userSurname, age)
 
     setUsersList((prevUserList) => {
-      return (
-        [...prevUserList, {
-          id: uuidv4(),
-          name: userName,
-          surname: userSurname,
-          age: age,
-        }]
+      const newUserList = [...prevUserList, {
+        id: uuidv4(),
+        name: userName,
+        surname: userSurname,
+        age: age,
+      }]
 
-      );
+      localStorage.setItem('users', JSON.stringify(newUserList))
+
+      return newUserList
+
     });
   };
 
   const deleteUserHandler = (id) => {
     console.log(id)
 
+    // filtered list without chosen element
     const filteredUser = usersList.filter((user) => {
       return user.id !== id
     })
 
+    localStorage.setItem('users', JSON.stringify(filteredUser))
     setUsersList(filteredUser)
   }
 
