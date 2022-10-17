6373e2ea815a2fb5780490a3db049eade19938b7
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-17 16:33:19 +0200

    delete unused import

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from '../UI/Button'
 import { Card } from '../UI/Card'
 import styled from './Tollbar.module.css'
